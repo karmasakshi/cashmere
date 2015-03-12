569e72a2cfe367128675fba52160d9ea6554c1d1
karmasakshi <karmasakshi@outlook.com>
2015-03-12 19:17:05 +0530

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cashmereApp
+ * @description
+ * # cashmereApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cashmereApp', []);
